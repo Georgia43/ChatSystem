0f48371ea6b741d46d7417f06738252c1786952a
TizzianaMansy <147388662+TizzianaMansy@users.noreply.github.com>
2023-11-07 14:17:04 +0100

    ajout de méthode newName

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -18,6 +18,11 @@ public class Library {
             Broadcast.sendFirstPacket(name);
         }
 
+        //changer son pseudo
+        public void newName(String newNickname) {
+           this.nickname= newNickname;
+        }
+
     }
     /*attributs*/
 
@@ -53,4 +58,5 @@ public class Library {
         return connectedUsers;
     }
 
+  
 }
