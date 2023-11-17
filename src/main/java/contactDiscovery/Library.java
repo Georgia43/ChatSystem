554f51f7d334d0a404bb73f5e571b7be86fbb4b4
Tizziana <Tizziana.Mansy@gmail.com>
2023-11-17 15:51:53 +0100

    sendcurrentNickname in library

--- src/main/java/contactDiscovery/Library.java
+++ src/main/java/contactDiscovery/Library.java
@@ -3,10 +3,17 @@ package contactDiscovery;
 import Model.AppData;
 import Model.User;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Library {
+
+    private static final Logger logger = Logger.getLogger("chatsystem");
 
     public Library() {
         //constructeur
@@ -21,7 +28,29 @@ public class Library {
     }
 
     //send nickname when chosen and unique
-    //for contact list -- send nickname a chaque pers
+    public static void SendCurrentNickname(String CurrentNickname){
+        Map<InetAddress, String> contactList = AppData.getContactList();
+
+        try {
+            DatagramSocket NameSocket = new DatagramSocket();
+            NameSocket.setBroadcast(true);
+
+            for (Map.Entry<InetAddress, String> pers : AppData.getContactList().entrySet()) {
+                InetAddress id = pers.getKey(); //get the ip address of the members of the contact list
+
+                String Nickname = "MY_NICKNAME_" + CurrentNickname;
+                byte[] NicknameMessage = Nickname.getBytes();
+                DatagramPacket NamePacket = new DatagramPacket(NicknameMessage, NicknameMessage.length, id, 4445);
+                NameSocket.send(NamePacket);
+            }
+            NameSocket.close();
+        }
+
+        catch (IOException e) {
+                logger.log(Level.SEVERE,"IOException: " + e.getMessage());
+        }
+    }
+
 
 
     //récupérer liste avec les utilisateurs connectés
