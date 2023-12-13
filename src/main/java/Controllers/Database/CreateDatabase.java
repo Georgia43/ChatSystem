1d5d65df5680e2bcad1192e8f91b9ebbcb25045b
georgia <koutsodi@insa-toulouse.fr>
2023-12-13 12:23:18 +0100

    create table messages

--- src/main/java/Controllers/Database/CreateDatabase.java
+++ src/main/java/Controllers/Database/CreateDatabase.java
@@ -1,4 +1,37 @@
 package Controllers.Database;
+import Controllers.Broadcast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+
 
 public class CreateDatabase {
+
+    public String url = "jdbc:sqlite:BDDchatsystem";
+    public static void tables(String username) throws SQLException {
+
+        try (Connection connection = DriverManager.getConnection(url);
+             Statement statement = connection.createStatement()){
+
+            String Users = "CREATE TABLE IF NOT EXISTS Users ("
+                    + "ipAdress INTEGER PRIMARY KEY, "
+                    + "name VARCHAR(20) NOT NULL"
+                    + "status INT NOT NULL)";
+            statement.executeUpdate(Users);
+
+
+            String Messages = "CREATE TABLE IF NOT EXISTS Messages_"+ username + "("
+                    + "content VARCHAR(100) PRIMARY KEY,"
+                    + "dateHeure DATETIME)";
+            statement.executeUpdate(Users);
+
+            System.out.println("Table 'Messages_' "+username+" créée.");
+        }
+        catch (SQLException e) {
+            Broadcast.logger.log(Level.SEVERE,"IOException: " + e.getMessage());
+        }
+    }
 }
