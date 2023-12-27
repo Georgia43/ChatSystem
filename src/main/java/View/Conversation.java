3de30724bdf9bc4404c4fd6e634622127648a816
georgia <koutsodi@insa-toulouse.fr>
2023-12-27 14:12:38 +0100

    conversation

--- src/main/java/View/Conversation.java
+++ src/main/java/View/Conversation.java
@@ -1,8 +1,37 @@
 package View;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import Controllers.UserInteraction;
+
 public class Conversation {
 
     public Conversation (String name, String ipaddress) {
+            JFrame frame = new JFrame("Conversartion with "+name);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            JFrame messageFrame = new JFrame("Messaging");
+            JTextArea messageArea = new JTextArea();
+            JTextField messageField = new JTextField();
+            JButton sendMessageButton = new JButton("Send");
+
+            sendMessageButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    String message = messageField.getText();
+                    UserInteraction inter = new UserInteraction();
+                    try {
+                        inter.sendMess(message,ipaddress);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    messageArea.append("You : "+message + "\n");
+                    messageField.setText("");
+                }
+            });
 
     }
 }
