d4f272078fac0335e22859588f5465360d22ba7b
Tizziana <Tizziana.Mansy@gmail.com>
2023-12-29 13:44:28 +0100

    "changed the fields"

--- src/main/java/View/Conversation.java
+++ src/main/java/View/Conversation.java
@@ -15,8 +15,23 @@ public class Conversation {
 
             JFrame messageFrame = new JFrame("Messaging");
             JTextArea messageArea = new JTextArea();
+            messageArea.setEditable(false);
             JTextField messageField = new JTextField();
             JButton sendMessageButton = new JButton("Send");
+
+        // Set layouts
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        JScrollPane scrollPane = new JScrollPane(messageArea);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        // Add components to the panel
+        panel.add(scrollPane);
+        panel.add(messageField);
+        panel.add(sendMessageButton);
+
+        // Add the panel to the frame
+        frame.getContentPane().add(panel);
 
             sendMessageButton.addActionListener(new ActionListener() {
                 @Override
@@ -32,6 +47,9 @@ public class Conversation {
                     messageField.setText("");
                 }
             });
+        frame.setSize(400, 400); // Set frame size
+        frame.setLocationRelativeTo(null); // Center the frame
+        frame.setVisible(true);
 
     }
 
