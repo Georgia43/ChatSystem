74b9145605d21d8561040e6feb8facbf9c2045e5
georgia <koutsodi@insa-toulouse.fr>
2024-01-14 12:11:24 +0100

    ReceiveTest marche

--- src/test/java/Controllers/ReceiveTest.java
+++ src/test/java/Controllers/ReceiveTest.java
@@ -1,5 +1,7 @@
 package Controllers;
 
+import Controllers.Database.CreateTables;
+import Controllers.Database.CreateTablesTest;
 import Model.AppData;
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -26,61 +28,74 @@ public class ReceiveTest extends TestCase
 
     //test de AddcontactList lorsque la personne n'est pas dans notre contact list
     public void testAddContactList() throws UnknownHostException {
-       /* InetAddress senderAddress = InetAddress.getByName("101.26.81.12");
+       InetAddress senderAddress = InetAddress.getByName("101.26.81.12");
         assert Library.GetConnectedUserList().isEmpty();
         AppData.addContactList(senderAddress, "Mary");
         System.out.println(Library.GetConnectedUserList());
         assert Library.GetConnectedUserList().size() == 1;
        assertEquals("User{username='Mary', id='/101.26.81.12'}", (Library.GetConnectedUserList().get(0).toString()));
        AppData.DeletefromContactList(senderAddress); //on vide la contact list à la fin de chaque test
-    */ assertTrue(true);
     }
 
 
     //test du handleReceived lorsqu'on reçoit un nickname
-    public void testReceiveNickname() throws IOException, SQLException {
-      /*  InetAddress senderAddress = InetAddress.getByName("100.26.81.12");
+    // TEST MARCHAIT AVANT DE PASSER A LA PHASE 2 ET DONC DE RAJOUTER DATABASE
+    /*public void testReceiveNickname() throws IOException, SQLException {
+       InetAddress senderAddress = InetAddress.getByName("100.26.81.12");
+        CreateTables dbTest = new CreateTables(CreateTablesTest.TestUrl);
         Broadcast.Receive.handleReceived(senderAddress,"MY_NICKNAME_John");
         System.out.println(Library.GetConnectedUserList());
         assertEquals("User{username='John', id='/100.26.81.12'}", (Library.GetConnectedUserList().get(0).toString()));
-        AppData.DeletefromContactList(senderAddress);*/ assertTrue(true);
-    }
+        AppData.DeletefromContactList(senderAddress);
+    }*/
 
     //test de AddcontactList lorsque on a déjà une personne avec ce nom dans notre contact liste
     public void testAddContactList_b() throws UnknownHostException {
-        /*InetAddress senderAddress = InetAddress.getByName("101.26.81.12");
+        InetAddress senderAddress = InetAddress.getByName("101.26.81.12");
         InetAddress senderAddress_2 = InetAddress.getByName("102.26.81.12");
         AppData.addContactList(senderAddress, "Mary");
         AppData.addContactList(senderAddress_2, "Mary");
         System.out.println(Library.GetConnectedUserList());
         assert Library.GetConnectedUserList().size() == 1; //on vérifie qu'on a bien 1 élément dans la liste et pas 2
-        AppData.DeletefromContactList(senderAddress);*/assertTrue(true);
+        AppData.DeletefromContactList(senderAddress);
     }
 
     //test pour enlever quelqu'un de notre contact list lorsqu'il n'est plus connecté
     public void testDeletefromContactList() throws UnknownHostException {
         //on ajoute une personne dans la contact list
-       /* InetAddress senderAddress = InetAddress.getByName("101.26.81.12");
+        InetAddress senderAddress = InetAddress.getByName("101.26.81.12");
         AppData.addContactList(senderAddress, "Mary");
         assert Library.GetConnectedUserList().size() == 1;
         System.out.println(Library.GetConnectedUserList());
         //on supprime la personne
         AppData.DeletefromContactList(senderAddress);
         System.out.println(Library.GetConnectedUserList());
-        assertTrue(Library.GetConnectedUserList().isEmpty());*/assertTrue(true);
+        assertTrue(Library.GetConnectedUserList().isEmpty());
     }
 
 
 
     //test du handleReceived lorsqu'on reçoit DISCONNECTING (lorsqu'un user veut se déconnecter, on l'enlève de notre contactlist)
     public void testReceiveDisconnecting() throws IOException, SQLException {
-      /*  InetAddress senderAddress = InetAddress.getByName("100.26.81.12");
+      InetAddress senderAddress = InetAddress.getByName("100.26.81.12");
         AppData.addContactList(senderAddress, "Mary");
         assert Library.GetConnectedUserList().size() == 1;
       System.out.println(Library.GetConnectedUserList());
         Broadcast.Receive.handleReceived(senderAddress,"DISCONNECTING");
         System.out.println(Library.GetConnectedUserList());
-        assert Library.GetConnectedUserList().isEmpty();*/
-        assertTrue(true);
+        assert Library.GetConnectedUserList().isEmpty();
+    }
+
+    //test du checkUnicityNickname
+
+    public void testCheckUnicityNickname() throws InterruptedException, UnknownHostException {
+        // Add a test entry to the contact list
+        InetAddress testInetAddress = InetAddress.getByName("127.0.0.1");
+        String testNickname = "TestUser";
+        AppData.addContactList(testInetAddress, testNickname);
+        // Test the method
+        boolean result = Broadcast.CheckUnicityNickname(testNickname);
+        // Verify that the result is as expected using an assertion
+        assertFalse(result); // The nickname "TestUser" should not be considered unique
     }
 }
