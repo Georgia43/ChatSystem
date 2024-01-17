690741556b1a67e1a8639a7ad524fa60d101925a
Georgia43 <121254429+Georgia43@users.noreply.github.com>
2024-01-17 16:49:42 +0100

    clean code User.java

--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -14,39 +14,14 @@ public class User {
         /*attributs*/
         private String nickname;
         private InetAddress id;
-
-        //private static Client client = new Client();
-
         private Socket clientSocket; // pour maintenir la connexion
 
 
         /*méthodes*/
         public String getNickname() {return this.nickname;}
+        
         public InetAddress getId() {return this.id;}
 
-   /* public static void recordConnectionSocket(Socket socket) {
-            // record a socket that canbe created
-          //  - when we establish ourselves
-          //  - when the other user connects to our global server socket
-        System.out.println("i am in recordConnectionSocket");
-       // clientSocket = socket;
-       // client.setSocket(clientSocket);
-        Server.ClientHandler clientHandler = new Server.ClientHandler(socket);
-        Server.clients.add(clientHandler);
-        clientHandler.start();
-    }*/
-
-     /* public static void addConnection(ServerSocket socket) {
-                try{
-                  // ServerSocket socket = new ServerSocket(Server.MESSAGE_PORT);
-                    System.out.println("!!!!!!!!!!!!!!!!!! i am in add connection");
-                    Socket socketAccepted = socket.accept();
-                    recordConnectionSocket(socketAccepted);
-
-                } catch (IOException e) {
-                        e.printStackTrace();
-              }
-        }*/
 
         public void setNickname(String name) {
             this.nickname= name;
